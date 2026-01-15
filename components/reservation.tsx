9bc2525299927be1c4ca5559ab7cbe72366a3ce4
crigaux <crigaux057@gmail.com>
2026-01-15 10:30:30 +0100

    Switch widget to light theme temporarily until transparent mode is fixed

--- components/reservation.tsx
+++ components/reservation.tsx
@@ -56,7 +56,7 @@ export function Reservation() {
           {/* Widget iframe */}
           <iframe
             id="tablepost-widget"
-            src="https://tablepost-khaki.vercel.app/widget/reservation/le-gourmet?theme=transparent&primary=D4AF37&shadow=none"
+            src="https://tablepost-khaki.vercel.app/widget/reservation/le-gourmet?theme=light&primary=D4AF37&radius=12&shadow=md"
             width="100%"
             frameBorder="0"
             allow="clipboard-write"
